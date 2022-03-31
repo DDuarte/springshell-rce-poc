fccf22f52884af741506230f884cea5480050ffb
Duarte Duarte <dnpd.dd@gmail.com>
2022-03-31 10:38:59 +0100

    no data binder imports needed

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,8 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.EvalBean;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
+//import org.springframework.web.bind.WebDataBinder;
+//import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +14,10 @@ public class IndexController {
 
     }
 
-    @InitBinder
-    public void initBinder(WebDataBinder binder) {
-
-    }
+    //@InitBinder
+    //public void initBinder(WebDataBinder binder) {
+    //    // mitigation:
+    //    String[] blackList = { "class.*", "Class.*", "*.class.*", ".*Class.*" };
+    //    binder.setDisallowedFields(blackList);
+    //}
 }
